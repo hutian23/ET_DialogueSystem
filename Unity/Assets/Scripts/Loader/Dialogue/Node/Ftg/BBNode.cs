8af07b7273c6efcf44fa25302ad04484cc1d629a
hutian123 <2504676118@qq.com>
2024-02-17 23:04:33 +0800

    导入拉格纳素材

---
+++ Unity/Assets/Scripts/Loader/Dialogue/Node/Ftg/BBNode.cs
@@ -0,0 +1,11 @@
+﻿using Sirenix.OdinInspector;
+
+namespace ET.Client
+{
+    [NodeType("Blazblue/技能节点")]
+    public class BBNode: DialogueNode
+    {
+        [FoldoutGroup("$nodeName/data")]
+        public string BBScript = "";
+    }
+}
---
+++ images/README.txt
@@ -0,0 +1,1 @@
+Assets extracted from the PC version of BlazBlue: Centralfiction by Y2K. No credit needed.
