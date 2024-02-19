fafc8fafd3945932e8de398060709645c381c53e
hutian123 <2504676118@qq.com>
2024-02-19 12:51:29 +0800

    ObjectWait不支持1对多,想想解决方案

---
+++ Unity/Assets/Scripts/Codes/HotfixView/Client/Demo/FightingGame/Hadoken_OPHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace ET.Client
+{
+    [ObjectSystem]
+    public class Hadoken_OPHandler
+    {
+        public async ETTask<Status> CheckHandler(Unit unit, ETCancellationToken token)
+        {
+            
+            await ETTask.CompletedTask;
+            return Status.Success;
+        }
+        // 120 SP
+        //必杀前置条件检测 + 按键检测
+        /* if SP < 60 return Status.Failed;
+         * FightingComponent fightingComponent = unit.GetComponent<FightingComponent>(token);
+         * wait = fightingComponent.GetComponent<ObjectWait>().Wait<Next_Input>();
+         *
+         * if(wait.OpType != OperaType.Up) return failed;
+         */
+    }
+}
---
+++ Unity/Assets/Scripts/Codes/ModelView/Client/Demo/FightingGame!!!/TOD_ObjectWait.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ET.Client
+{
+    [ComponentOf]
+    public class TODObjectWait: Entity, IAwake, IDestroy, ILoad
+    {
+        public Dictionary<Type, object> tcss = new();
+    }
+
+    public interface TODIDestroyRun
+    {
+        void SetResult();
+    }
+
+    public class ResultCallback<K>: TODIDestroyRun where K : struct, IWaitType
+    {
+        private ETTask<K> tcs;
+
+        public ResultCallback()
+        {
+            this.tcs = ETTask<K>.Create(true);
+        }
+
+        public bool IsDisposed
+        {
+            get
+            {
+                return this.tcs == null;
+            }
+        }
+
+        public ETTask<K> Task => this.tcs;
+
+        public void SetResult(K k)
+        {
+            var t = this.tcs;
+            this.tcs = null;
+            t.SetResult(k);
+        }
+
+        public void SetResult()
+        {
+            var t = this.tcs;
+            this.tcs = null;
+            t.SetResult(new K() { Error = WaitTypeError.Destroy });
+        }
+    }
+}
