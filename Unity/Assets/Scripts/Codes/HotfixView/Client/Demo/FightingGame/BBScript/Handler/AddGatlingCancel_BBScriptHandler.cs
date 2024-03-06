f1c94ebd2d30d04e1bf8b1db3d96ab0a5713809a
hutian123 <2504676118@qq.com>
2024-03-06 10:35:56 +0800

    记录一下预输入的思路
    AddGatlingCancel skill = Sol_5HS;
    
    之前参照b站up主刘笑寒的视频,指令有缓冲之外，当执行一个行为时，其他行为也会被缓冲，当可以GC的时候取出来，判断满足前置条件吗，满足，则执行这个行为
    例如 JP 2626HP 满足SA1和重风神的指令条件，两个行为都被缓冲了.如果有必杀槽，可以释放SA1; 如果没有，则变成了重风神
    
    我的思路:
    1. 不关闭输入检测协程，每帧检测有无符合条件的行为，有，则缓冲这个行为
    2. 栈 缓冲行为(后进先出) 行为缓冲数据结构中需要包含缓冲行为的帧号，顺序(例如，SA比必杀顺序高，相同必杀中，PO的指令投顺序比其他必杀高)
    3. 可GC取消时(设置一个窗口)，每帧遍历栈，找到符合条件的行为(包括数值检测等等),执行

--- Unity/Assets/Scripts/Codes/HotfixView/Client/Demo/FightingGame/BBScript/Handler/AddGatlingCancel_BBScriptHandler.cs
+++ Unity/Assets/Scripts/Codes/HotfixView/Client/Demo/FightingGame/BBScript/Handler/AddGatlingCancel_BBScriptHandler.cs
@@ -10,6 +10,13 @@ namespace ET.Client
         }
 
         //AddGatlingCancel skill = Sol_5HS;
+        //之前参照b站up主刘笑寒的视频,指令有缓冲之外，当执行一个行为时，其他行为也会被缓冲，当可以GC的时候取出来，判断满足前置条件吗，满足，则执行这个行为
+        //例如 JP 2626HP 满足SA1和重风神的指令条件，两个行为都被缓冲了.如果有必杀槽，可以释放SA1; 如果没有，则变成了重风神
+        //
+        //我的思路:
+        //1. 不关闭输入检测协程，每帧检测有无符合条件的行为，有，则缓冲这个行为
+        //2. 栈 缓冲行为(后进先出) 行为缓冲数据结构中需要包含缓冲行为的帧号，顺序(例如，SA比必杀顺序高，相同必杀中，PO的指令投顺序比其他必杀高)
+        //3. 可GC取消时(设置一个窗口)，每帧遍历栈，找到符合条件的行为(包括数值检测等等),执行
         public override async ETTask<Status> Handle(Unit unit, string opCode, ETCancellationToken token)
         {
             Match match = Regex.Match(opCode, @"AddGatlingCancel skill = (?<skill>\w+);");
